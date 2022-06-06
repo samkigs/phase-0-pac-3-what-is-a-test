f9ad985970940c41080402bc93c57adc65e9bedd
samkigs <samuel.kihuga@student.moringaschool.com>
2022-06-06 12:57:02 +0300

    Test

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-const name = "Joe";
-const height = 74;
-const message = `${name} is ${height} inches tall`;
+const { name, height, message } = require("../index.js");
 
-module.exports = {name, height, message};
+
+describe('what-is-a-test', () => {
+  describe('Name', () => {
+    it('returns "Susan"', () => {
+      expect(name).toEqual('Susan')
+    })
+  })
+
+
+  describe('Height', () => {
+    it('is less than 40', () => {
+      expect(height).toBeLessThan(40)
+    })
+  })
+
+  describe('Message', () => {
+    it('gives the name and height', () => {
+      expect(message).toInclude(name)
+      expect(message).toInclude(height)
+    })
+  })
+})
